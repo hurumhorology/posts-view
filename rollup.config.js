88445284b3034b9d326f634ba2439328fb1b21d8
pyo-sh <pygosky@gmail.com>
2024-03-22 15:33:05 +0900

    feat: Rollup 설정 및 프로젝트 빌드 환경 구성

---
+++ rollup.config.js
@@ -0,0 +1,43 @@
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import resolve from "@rollup/plugin-node-resolve";
+import terser from "@rollup/plugin-terser";
+import typescript from "@rollup/plugin-typescript";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
+import packageJson from "./package.json";
+
+const configs = [
+  {
+    input: "src/index.ts",
+    output: [
+      {
+        file: packageJson.main,
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      babel({
+        babelHelpers: "bundled",
+        presets: [
+          "@babel/preset-env",
+          "@babel/preset-react",
+          "@babel/preset-typescript",
+        ],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      }),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      terser(),
+    ],
+  },
+];
+
+export default configs;

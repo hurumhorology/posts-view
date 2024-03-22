9a1dcbab46c5f9d9937b798f612d89d71f0a642a
pyo-sh <pygosky@gmail.com>
2024-03-22 17:38:24 +0900

    feat: Component 개발을 위한 Storybook 설치 및 환경 구축

---
+++ .storybook/main.ts
@@ -0,0 +1,49 @@
+import type { StorybookConfig } from "@storybook/react-vite";
+import path from "path";
+import aliasJson from "../tsconfig.alias.json";
+
+const defineEntries = () => {
+  return Object.entries(aliasJson.compilerOptions.paths).reduce(
+    (acc, [from, [to]]) => {
+      const key = from.replace(new RegExp("\\/\\*", "g"), "");
+      const value = path.resolve(
+        __dirname,
+        "..",
+        to.replace(new RegExp("\\/\\*", "g"), ""),
+      );
+      acc[key] = value;
+      return acc;
+    },
+    {},
+  );
+};
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  docs: {
+    autodocs: "tag",
+  },
+  async viteFinal(config) {
+    config.resolve = {
+      alias: defineEntries(),
+    };
+    config.define = {
+      ...(config?.define ?? {}),
+      "process.env.NODE_DEBUG": false,
+    };
+
+    return config;
+  },
+};
+export default config;

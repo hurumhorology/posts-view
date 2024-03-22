36363e703ed341dc772bde8054a825d5ab9e2298
pyo-sh <pygosky@gmail.com>
2024-03-22 16:17:23 +0900

    fix: Rollup Bundling 후 d.ts 파일이 하나의 파일로 나오도록 설정

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,19 @@ import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import packageJson from "./package.json";
+import alias from "@rollup/plugin-alias";
+import aliasJson from "./tsconfig.alias.json";
+import path from "path";
+import { dts } from "rollup-plugin-dts";
+
+const defineEntries = () => {
+  return Object.entries(aliasJson.compilerOptions.paths).map(([from, [to]]) => {
+    return {
+      find: from.replaceAll("/*", ""),
+      replacement: path.resolve(__dirname, to.replaceAll("/*", "")),
+    };
+  });
+};
 
 const configs = [
   {
@@ -34,8 +47,21 @@ const configs = [
         ],
         extensions: [".js", ".jsx", ".ts", ".tsx"],
       }),
-      typescript({ tsconfig: "./tsconfig.json" }),
+      typescript(),
       terser(),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: [
+      { file: "build/esm/index.d.ts", format: "es" },
+      { file: "build/cjs/index.d.ts", format: "cjs" },
+    ],
+    plugins: [
+      dts(),
+      alias({
+        entries: defineEntries(),
+      }),
     ],
   },
 ];

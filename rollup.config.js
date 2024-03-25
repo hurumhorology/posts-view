21785577554f43c6893e6af5e5d1a2921aa0134d
pyo-sh <pygosky@gmail.com>
2024-03-25 16:48:58 +0900

    feat: Multiple import와 함께 Styles output 초기 설정

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import { existsSync, mkdirSync, writeFileSync } from "fs";
 import path from "path";
 import alias from "@rollup/plugin-alias";
 import babel from "@rollup/plugin-babel";
@@ -8,9 +9,33 @@ import typescript from "@rollup/plugin-typescript";
 import { dts } from "rollup-plugin-dts";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import scss from "rollup-plugin-scss";
-import packageJson from "./package.json";
+import multiInput from "./rollup.input.js";
 import aliasJson from "./tsconfig.alias.json";
 
+const INPUTS = "src/**/index.ts";
+const OUTPUT_DIR = "build";
+
+const defineStyleOutputs = (styles, styleNodes) => {
+  Object.entries(styleNodes ?? {}).forEach(([fileName, style]) => {
+    // change src -> build
+    const fileRelativePath = path.relative(
+      "src",
+      fileName.slice(0, fileName.length - path.extname(fileName).length),
+    );
+    const filePath = path.resolve(__dirname, OUTPUT_DIR, fileRelativePath);
+    const dirPath = path.dirname(filePath);
+    const buildPath = path.resolve(dirPath, "index.css");
+
+    // should create directory before write file
+    if (!existsSync(dirPath)) {
+      mkdirSync(dirPath, { recursive: true });
+    }
+
+    writeFileSync(buildPath, style);
+  });
+};
+
+// To change d.ts alias path via tsconfig.alias.json
 const defineEntries = () => {
   return Object.entries(aliasJson.compilerOptions.paths).map(([from, [to]]) => {
     return {
@@ -22,24 +47,20 @@ const defineEntries = () => {
 
 const configs = [
   {
-    input: "src/index.ts",
+    input: INPUTS,
     output: [
       {
-        file: packageJson.main,
-        format: "cjs",
-        sourcemap: true,
-      },
-      {
-        file: packageJson.module,
+        dir: OUTPUT_DIR,
         format: "esm",
         sourcemap: true,
       },
     ],
     plugins: [
+      multiInput(),
+      scss({ output: defineStyleOutputs }),
       peerDepsExternal(),
       resolve(),
       commonjs(),
-      scss(),
       babel({
         babelHelpers: "bundled",
         presets: [
@@ -54,13 +75,11 @@ const configs = [
     ],
   },
   {
-    input: "src/index.ts",
-    output: [
-      { file: "build/esm/index.d.ts", format: "es" },
-      { file: "build/cjs/index.d.ts", format: "cjs" },
-    ],
+    input: INPUTS,
+    output: [{ dir: OUTPUT_DIR, format: "esm" }],
     external: [/\.(css|sass|scss)$/],
     plugins: [
+      multiInput(),
       dts(),
       alias({
         entries: defineEntries(),

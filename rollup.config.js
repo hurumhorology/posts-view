a43a4d0b6fe8a2f4abbb4726f544a39e38b795f4
pyo-sh <pygosky@gmail.com>
2024-03-26 16:29:52 +0900

    fix: Rollup에서 SCSS 파일이 각 css 파일로 bundle 된 후 js 파일에서 이를 import 하도록 변경

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,3 @@
-import { existsSync, mkdirSync, writeFileSync } from "fs";
 import path from "path";
 import alias from "@rollup/plugin-alias";
 import babel from "@rollup/plugin-babel";
@@ -8,31 +7,11 @@ import terser from "@rollup/plugin-terser";
 import typescript from "@rollup/plugin-typescript";
 import { dts } from "rollup-plugin-dts";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
-import scss from "rollup-plugin-scss";
+import style, { onwarn as onWarnStyle } from "./rollup.style";
 import aliasJson from "./tsconfig.alias.json";
 
 const INPUT = "src/index.ts";
 const OUTPUT_DIR = "build";
-
-const defineStyleOutputs = (styles, styleNodes) => {
-  Object.entries(styleNodes ?? {}).forEach(([fileName, style]) => {
-    // change src -> build
-    const fileRelativePath = path.relative(
-      "src",
-      fileName.slice(0, fileName.length - path.extname(fileName).length),
-    );
-    const filePath = path.resolve(__dirname, OUTPUT_DIR, fileRelativePath);
-    const dirPath = path.dirname(filePath);
-    const buildPath = path.resolve(dirPath, "index.css");
-
-    // should create directory before write file
-    if (!existsSync(dirPath)) {
-      mkdirSync(dirPath, { recursive: true });
-    }
-
-    writeFileSync(buildPath, style);
-  });
-};
 
 // To change d.ts alias path via tsconfig.alias.json
 const defineEntries = () => {
@@ -51,13 +30,18 @@ const configs = [
       {
         dir: OUTPUT_DIR,
         format: "esm",
-        sourcemap: true,
         preserveModules: true,
         preserveModulesRoot: "src",
       },
     ],
+    onwarn: (...args) => {
+      onWarnStyle(...args);
+    },
     plugins: [
-      scss({ output: defineStyleOutputs }),
+      style({
+        removePath: "src",
+        scopedName: "[local]",
+      }),
       peerDepsExternal(),
       resolve(),
       commonjs(),
